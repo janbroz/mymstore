a4eff9c5e20dc6060693315e2ce45e7f13d6907f
Juan Broz <f870421@gmail.com>
2016-11-08 04:50:51 -0500

    repuesto en sede service

--- app/services/repuestoensede_service.rb
+++ app/services/repuestoensede_service.rb
@@ -5,8 +5,21 @@ class RepuestoensedeService
   end
 
   def consultar_repuesto_en_sede
+
+  end
+  
+  def agregar_cantidad(sede, repuesto, cantidad)
+    response = @client.call(:agregarCantidad, message: {"id_sede" => sede, "id_repuesto" => repuesto, "CantidadRepuesto" => cantidad})
   end
 
+  def sumar_cantidad(idsede, idrepuesto, sCantidad)
+    response = @client.call(:sumarCantidad, message: {"idsede" => idsede, "idrepuesto" => idrepuesto, "sCantidad" => sCantidad})
+  end
+
+  def restar_cantidad(idsede, idrepuesto, sCantidad)
+    response = @client.call(:restarCantidad, message: {"idsede" => idsede, "idrepuesto" => idrepuesto, "sCantidad" => sCantidad})
+  end
+  
   def modificar_repuesto_en_sede
   end
 
